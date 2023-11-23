0e0dcd4d2eb76bd2c9335fca8cc7c5f1001a2969
Lucas Farias <lukf95@gmail.com>
2023-11-23 23:47:16 +0100

    Roll back stuff

--- src/shared/components/layout/Layout.tsx
+++ src/shared/components/layout/Layout.tsx
@@ -1,34 +1,15 @@
 import classnames from 'classnames';
 import { useContext } from 'react';
 import { DarkModeContext } from '@/shared/contexts/DarkModeContext';
-import PacklifyLogo from '../packlify-logo/PacklifyLogo';
 
-export default function Layout({ children }: ILayoutProps) {
+export default function Layout({ children }: IComponent) {
   const darkMode = useContext(DarkModeContext);
-
-  // const Navbar = navbar ? <div>Navbar</div> : null;
 
   return (
     <>
       <main className={classnames({ dark: darkMode })}>{children}</main>
-      <footer
-        className={classnames('h-100 border-t bg-white p-6 text-center text-xs text-gray-400 shadow-sm', {
-          dark: darkMode,
-        })}
-      >
-        <div className="flex items-center text-2xl brand justify-center mb-2 text-black/70">
-          <PacklifyLogo className="text-[30px] mr-2" />
-          Packlify
-        </div>
-
-        <div>
-          Copyright &copy; {new Date().getFullYear()} Lucas Farias. All rights reserved. <p>lucasfarias.com</p>
-        </div>
-      </footer>
     </>
   );
 }
 
-interface ILayoutProps extends IComponent {
-  navbar?: boolean;
-}
+// interface ILayoutProps extends IComponent {}
--- src/shared/views/cloud/pages/Create.tsx
+++
@@ -1,9 +0,0 @@
-import Header from '@/shared/components/header/Header';
-
-export default function Create() {
-  return (
-    <div>
-      <Header title="Create" subtitle="Fill our the information to get started with a new project" />
-    </div>
-  );
-}
