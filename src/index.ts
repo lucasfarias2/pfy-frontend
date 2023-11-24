812c372f0eb37218b05f04a149773429bb194d6f
Lucas Farias <lukf95@gmail.com>
2023-11-24 19:52:25 +0100

    Fix assets

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ const port = process.env.PORT || 3000;
     app.use(express.json());
     app.use(favicon('public/favicon.ico'));
     app.use(cookieParser());
+    app.use('/', express.static('public'));
 
     app.use('/api', apiRouter);
 
